285ec896b0cb41db0954ae4658a69171853e75c1
touhidalam09 <touhidalam649@gmail.com>
2021-10-03 02:19:27 +0600

    create file design

--- src/pages/Creact.jsx
+++ src/pages/Creact.jsx
@@ -1,10 +1,44 @@
+import { makeStyles } from '@material-ui/styles'
+import { Button, Container, TextField, Typography } from '@mui/material'
+import { Box } from '@mui/system'
 import React from 'react'
 
+const useStyles = makeStyles({
+    textField: {
+        display: 'block',
+        marginBottom: "20px"
+    }
+})
+
 function Creact() {
+    const classes = useStyles()
     return (
-        <div>
-           Create 
-        </div>
+        <Container>
+            <Typography variant="h4" color="textSecondary">Create a new note</Typography>
+            <Box component="form" noValidate autoComplete="off">
+                <div className={classes.textField}>
+                    <TextField
+                        variant="outlined"
+                        label="Title"
+                        required
+                        fullWidth
+                        margin="normal"
+                    />
+                    <TextField
+                        variant="outlined"
+                        label="Details"
+                        required
+                        multiline
+                        rows={4}
+                        fullWidth
+                        margin="normal"
+                    />
+                </div>
+                <div>
+                    <Button type="submit" variant="contained" >Submit</Button>
+                </div>
+            </Box>
+        </Container>
     )
 }
 
